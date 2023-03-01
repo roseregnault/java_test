67bfd3e739e1b02fcb2c7c829fcf48b658328569
LAPTOP-P4GHLRM2\rlgomez_profile <rlgomez@profile.es>
2023-03-01 20:26:17 +0100

    Test unitarios ultimos casos

--- src/test/java/PricesControllerTest.java
+++ src/test/java/PricesControllerTest.java
@@ -43,4 +43,43 @@ public class PricesControllerTest {
                 .andExpect(jsonPath("$.price_list").value("25.45"))
                 .andExpect(jsonPath("$.price").value("28.95"));
     }
+
+    @Test
+    public void test3() throws Exception {
+        mockMvc.perform(get("/prices")
+                        .param("start_date", "2023-03-14T21:00:00Z")
+                        .param("product_id", "35455")
+                        .param("brand_id", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.product_id").value("35455"))
+                .andExpect(jsonPath("$.brand_id").value("1"))
+                .andExpect(jsonPath("$.price_list").value("25.45"))
+                .andExpect(jsonPath("$.price").value("28.95"));
+    }
+
+    @Test
+    public void test4() throws Exception {
+        mockMvc.perform(get("/prices")
+                        .param("start_date", "2023-03-15T10:00:00Z")
+                        .param("product_id", "35455")
+                        .param("brand_id", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.product_id").value("35455"))
+                .andExpect(jsonPath("$.brand_id").value("1"))
+                .andExpect(jsonPath("$.price_list").value("25.45"))
+                .andExpect(jsonPath("$.price").value("28.95"));
+    }
+
+    @Test
+    public void test5() throws Exception {
+        mockMvc.perform(get("/prices")
+                        .param("start_date", "2023-03-16T21:00:00Z")
+                        .param("product_id", "35455")
+                        .param("brand_id", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.product_id").value("35455"))
+                .andExpect(jsonPath("$.brand_id").value("1"))
+                .andExpect(jsonPath("$.price_list").value("25.45"))
+                .andExpect(jsonPath("$.price").value("28.95"));
+    }
 }
